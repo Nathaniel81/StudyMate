99e0b55ce375c4f24df686fa913a4f7c9c17cd94
Nathaniel81 <nathanield356@gmail.com>
2023-06-24 09:17:18 +0300

    Enhancing code readability by adding a docstring

--- base/views.py
+++ base/views.py
@@ -8,9 +8,21 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 # from django.contrib.auth.forms import UserCreationForm
-
+"""
+This is a Django web application with several views defined in the code. 
+The views are responsible for rendering the appropriate HTML templates 
+when a user makes a request to the server. 
+The application supports user registration and login, as well as the creation, 
+updating, and deletion of chat rooms and messages.
+"""
 
 def loginPage(request):
+    """
+    Renders the login page when the user navigates to the URL specified in the urlpattern. 
+    If the user is already authenticated, they will be redirected to the home page. 
+    If the user submits the login form, the view will attempt to authenticate the user 
+    with the provided credentials, and redirect to the home page if successful.
+    """
     page = 'login'
     if request.user.is_authenticated:
         return redirect('home')
@@ -33,10 +45,17 @@ def loginPage(request):
     return render(request, 'base/login_register.html', context)
 
 def logoutUser(request):
-    logout(request)
-    return redirect('home')
+     """Logs out the current user and redirects them to the home page."""
+     logout(request)
+     return redirect('home')
 
 def registerPage(request):
+    """
+    Renders the registration page when the user navigates to the URL specified in the urlpattern.
+    If the user submits the registration form, the view will attempt to create a new user with the 
+    provided information and log them in if successful.
+    """
+
     form = MyUserCreationForm()
     # form = UserForm()
 
@@ -54,6 +73,11 @@ def registerPage(request):
     return render(request, 'base/login_register.html', {'form': form})
 
 def home(request):
+    """
+    Renders the home page, which displays a list of chat rooms and recent messages. 
+    The view filters the chat rooms based on a search query submitted by the user, 
+    and limits the number of topics and messages displayed.
+    """
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     rooms = Room.objects.filter(Q(topic__name__icontains=q) |
                                 Q(name__icontains=q) |
@@ -65,6 +89,11 @@ def home(request):
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
+    """
+    Renders the chat room page, which displays a list of messages and participants in the room. 
+    If the user submits a message, the view will create a new message and add the user to the list
+    of participants in the room.
+    """
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all()#.order_by('-created')
     participants = room.participants.all()
@@ -82,6 +111,10 @@ def room(request, pk):
     return render(request, 'base/room.html', context)
 
 def userProfile(request, pk):
+    """
+    Renders the user profile page, which displays information about the user 
+    and their chat room and message history.
+    """
     user = User.objects.get(id=pk)
     user_bio = user.bio
     rooms = user.room_set.all()
@@ -92,6 +125,11 @@ def userProfile(request, pk):
 
 @login_required(login_url='login')
 def createRoom(request):
+    """
+    Renders the chat room creation form. If the user submits the form, 
+    the view will create a new chat room with the provided information.
+    """
+
     topics = Topic.objects.all()
     form = RoomForm()
     if request.method == 'POST':
@@ -116,6 +154,11 @@ def createRoom(request):
 
 @login_required(login_url='login')
 def UpdateRoom(request, pk):
+    """
+    Renders the chat room update form. If the user submits the form, 
+    the view will update the chat room with the provided information.
+    """
+
     topics = Topic.objects.all()
     room = Room.objects.get(id=pk)
     form = RoomForm(request.POST, instance=room)
@@ -139,6 +182,7 @@ def UpdateRoom(request, pk):
 
 @login_required(login_url='login')
 def DeleteRoom(request, pk):
+    """Deletes the specified message"""
     obj = Room.objects.get(id=pk)
     room = Room.objects.get(id=pk)
 
@@ -152,6 +196,10 @@ def DeleteRoom(request, pk):
 
 @login_required(login_url='login')
 def deleteMessage(request, pk):
+    """
+    Deletes the specified message and redirects the user back to the chat room. 
+    Only the user who created the message is allowed to delete it.
+    """
     message = Message.objects.get(id=pk)
 
     if request.user != message.user:
@@ -164,6 +212,7 @@ def deleteMessage(request, pk):
 
 @login_required(login_url='login')
 def updateUser(request):
+    """View function to update the current user's profile."""
     user = request.user
     form = UserForm(instance=user)
 
@@ -176,11 +225,13 @@ def updateUser(request):
     return render(request, 'base/update-user.html', {'form' : form})
 
 def topicsPage(request):
+    """Renders the topics page that displays all the available topics."""
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     topics = Topic.objects.filter(name__icontains=q)
     return render(request, 'base/topics.html', {'topics': topics})
 
 def activityPage(request):
+    """Renders the activity page that displays all the user's activity."""
     # messages = Message.objects.all()
     # room = Room.objects.filter()
     room_messages = Message.objects.all()
