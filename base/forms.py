8338313f0217076d4b214a31c4cc9e51898b44f7
Nathaniel81 <nathanield356@gmail.com>
2023-06-24 09:26:57 +0300

    Increased code clarity

--- base/forms.py
+++ base/forms.py
@@ -3,19 +3,30 @@ from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from .models import Room, User
 
+
+"""forms module"""
 class MyUserCreationForm(UserCreationForm):
+    """
+    A form for creating a new user, extending Django's built-in UserCreationForm
+    and adding the 'name' field.
+    """
     class Meta:
         model = User
         fields = ['name', 'username', 'email', 'password1', 'password2']
 
 class RoomForm(ModelForm):
+    """
+    A form for creating or updating a Room instance.
+    """
     class Meta:
         model = Room
         fields = '__all__'
         exclude = ['host', 'participants']
 
 class UserForm(ModelForm):
+    """
+    A form for creating or updating a User instance.
+    """
     class Meta:
         model = User
         fields = ['avatar', 'name', 'username', 'email', 'bio']
- 
